ab241f81a4e33b3ce3095b4a86decf81a448c55c
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-05 16:39:07 +0900

    181 配列の要素はクラスの違うものでも良い

--- note.rb
+++ note.rb
@@ -8,3 +8,11 @@ puts array
 [1, 2, 3, 4, 5].each do |n|
   puts n if n.odd?
 end
+
+['千葉', '秋田'].each do |pref|
+  puts "ここは#{ pref }県です"
+end
+
+['千葉', nil].each do |obj|
+  puts "#{ obj.class } obj.class"
+end
