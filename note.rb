8f2942c61215d65e6517017f44cbbc102c1a09b9
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-05 13:53:26 +0900

    148 utf-8とshift_jisで区切り文字が違う

--- note.rb
+++ note.rb
@@ -11,10 +11,11 @@ puts "#{ 60 * 1}"
 str = 'オブジェクト指向スクリプト'
 sout = str[-5,3]
 puts sout
-
 puts "FooFar".upcase
 puts "FooFar".downcase
-
 puts "[%10d]" % 10
 puts "[%010d]" % 10
 puts "[%010s]" % 10
+puts 'あ'.encoding
+puts 'あ'.encode('UTF-8').bytes
+puts 'あ'.encode('Shift_JIS').bytes
