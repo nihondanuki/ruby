1e1a654e4423368bb935cb3eac239ba72f2e801f
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-06 17:09:02 +0900

    316 Time

--- note.rb
+++ note.rb
@@ -1,22 +1,2 @@
-begin
-  puts 'hi'
-  {}.fetch('foo')
-rescue
-  puts 'rescue'
-else
-  puts 'else'
-ensure
-  puts 'ensure'
-end
-
-begin
-  exit
-rescue SystemExit => e
-  puts "exception class -> #{ e.class }"
-end
-
-class SampleError < StandardError;end
-
-begin
-  raise SampleError
-end
+puts Time.new
+puts Time.now
