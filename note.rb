e720a45c464f2c88daee58de5cb4c0febb9f7fad
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-06 17:34:13 +0900

    327 file

--- note.rb
+++ note.rb
@@ -1,2 +1,27 @@
 puts Time.new
 puts Time.now
+
+
+f = File.open("./note.txt","w")
+f.puts 'hello'
+f.close
+
+f = File.open("./note.txt","r")
+puts f.read
+f.close
+
+File.open("./note.txt","w") do |f|
+  f.write 'kinonotabi'
+end
+
+File.open("./note.txt","r") do |f|
+  puts f.read
+end
+
+puts File.read("./note.txt")
+
+
+
+File.readlines("./note.txt").each do |line|
+  puts line
+end
