04d681d89ad6b22f5c93d7cd031f1d5b19ea7245
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-06 16:26:00 +0900

    448

--- note.rb
+++ note.rb
@@ -9,3 +9,32 @@ class Sample
 end
 
 Sample.mixin_method
+
+module Foo
+  module Bar
+    class Sample
+      MESSAGE = 'hello'
+    end
+  end
+end
+
+puts ::Foo::Bar::Sample::MESSAGE
+
+module Foo
+  def foo_method
+    puts "self => #{ self }"
+  end
+end
+
+class Inc
+  include Foo
+end
+
+class Ext
+  extend Foo
+end
+
+inc = Inc.new
+inc.foo_method
+
+Ext.foo_method
