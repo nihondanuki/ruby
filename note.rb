47da0dc5d5551202357108d9b949c69778ea4574
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-09 13:29:21 +0900

    yaml

--- note.rb
+++ note.rb
@@ -59,4 +59,7 @@ puts data
 
 JSON.parse(File.read('./json_sample.json'), symbolize_names: true)
 
-
+require 'yaml'
+array = ['array', 'array2']
+data = { key1: "value", key2: "value", key3: array }
+puts YAML.dump(data)
