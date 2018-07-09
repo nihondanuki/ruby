d056a6819fc12ba023990021b6cb7a76113dd588
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-09 11:07:02 +0900

    341 問題ない

--- note.rb
+++ note.rb
@@ -3,3 +3,13 @@ require 'time'
 t = Time.parse("2018/03/24 14:03:00")
 
 t.class
+
+Time.now
+
+Time.parse("23:00:00")
+
+Time.parse("3/5")
+
+Time.parse("23:00:00", Time.new(2010, 2, 10))
+
+Time.parse("3/5", Time.new(2010, 2, 10))
