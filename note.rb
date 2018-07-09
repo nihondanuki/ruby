7a37c7620b3eb725fd4e7dc0f903b08c03991788
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-09 12:59:51 +0900

    344 ブロックをよく使う

--- note.rb
+++ note.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'csv'
 
 t = Time.parse("2018/03/24 14:03:00")
 
@@ -21,3 +22,19 @@ puts t.rfc2822
 t = Time.rfc2822("Sat, 24 Mar 2018 14:07:04 +0900")
 
 puts t.class
+
+CSV.foreach('./a.csv') do |row|
+  puts "row -> #{ row }"
+end
+
+CSV.open('./a.csv', 'w') do |csv|
+  csv << ['foo', 'bar']
+  csv << [1, 2]
+end
+
+str_csv = CSV.generate do |csv|
+  csv << ['foo', 'bar']
+  csv << [1, 2]
+end
+
+puts str_csv
