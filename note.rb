21ab5698ac5c3be659194e6a59b030a0b122d557
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-09 11:15:32 +0900

    342　フォーマットメソッドの追加

--- note.rb
+++ note.rb
@@ -13,3 +13,11 @@ Time.parse("3/5")
 Time.parse("23:00:00", Time.new(2010, 2, 10))
 
 Time.parse("3/5", Time.new(2010, 2, 10))
+
+puts t = Time.now
+
+puts t.rfc2822
+
+t = Time.rfc2822("Sat, 24 Mar 2018 14:07:04 +0900")
+
+puts t.class
