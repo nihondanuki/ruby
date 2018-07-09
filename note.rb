8d0d1041d63cc03389aafc179ad33d6693aff1f0
takuotuka <otsuka.taku@mwed.co.jp>
2018-07-09 16:56:43 +0900

    hash.newのブロックに渡すものはkey valueじゃないだと

--- note.rb
+++ note.rb
@@ -1,3 +1,24 @@
-str = "message".freeze
+message = 'hello'
+puts "My message = \"#{ message }\""
+puts %Q!My message = "#{ message }"!
 
-str.reverse!
+a = %w!東京都 a!
+puts a
+
+%i!foo bar!
+
+Array(nil)
+Array('test')
+
+Array(['foo', 'bar'])
+
+Array.new(3)
+
+Array.new(3,'a')
+
+array = Array.new(3){ 'a' }
+
+Hash.new do |h, k|
+  puts h
+  puts k
+end
